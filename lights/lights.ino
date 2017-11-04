3f14df2a195cca1ffc5a6f6724bf9bba9bf9c195
matt k <cottsak@gmail.com>
2017-11-04 14:30:10 +0800

    using fets 2 and 3 for alternating 'police LED' flasher function array isn't the cleanest way to avoid the huge delay in making the flashing sequence in one function but it will do for now

--- lights/lights.ino
+++ lights/lights.ino
@@ -15,6 +15,24 @@ const int outputPinFet2 = 11;
 bool fet3High = false;
 const int outputPinFet3 = 12;
 
+bool celingLightsEnabled = false;
+bool emergencyLightsEnabled = false;
+
+void elF1() { digitalWrite(outputPinFet2, HIGH); delay(50); digitalWrite(outputPinFet2, LOW); delay(50); }
+void elF2() { digitalWrite(outputPinFet2, HIGH); delay(50); digitalWrite(outputPinFet2, LOW); delay(50); }
+void elF3() { digitalWrite(outputPinFet2, HIGH); delay(50); digitalWrite(outputPinFet2, LOW); delay(50); }
+void elF4() { digitalWrite(outputPinFet2, HIGH); delay(50); digitalWrite(outputPinFet2, LOW); delay(50); }
+void elF5() { digitalWrite(outputPinFet2, HIGH); delay(50); digitalWrite(outputPinFet2, LOW); delay(50); }
+void elF6() { delay(300); }
+void elF7() { digitalWrite(outputPinFet3, HIGH); delay(50); digitalWrite(outputPinFet3, LOW); delay(50); }
+void elF8() { digitalWrite(outputPinFet3, HIGH); delay(50); digitalWrite(outputPinFet3, LOW); delay(50); }
+void elF9() { digitalWrite(outputPinFet3, HIGH); delay(50); digitalWrite(outputPinFet3, LOW); delay(50); }
+void elF10() { digitalWrite(outputPinFet3, HIGH); delay(50); digitalWrite(outputPinFet3, LOW); delay(50); }
+void elF11() { digitalWrite(outputPinFet3, HIGH); delay(50); digitalWrite(outputPinFet3, LOW); delay(50); }
+void elF12() { delay(300); }
+void  (*emergencyLightsSequenceFP[])() = { elF1, elF2, elF3, elF4, elF5, elF6, elF7, elF8, elF9, elF10, elF11, elF12 };
+int emergencyLightsSequenceIndex = 0;
+
 void setup() {
   Serial.begin(9600);
 
@@ -26,21 +44,21 @@ void setup() {
   pinMode(outputPinFet3, OUTPUT);
 
   Serial.println("ready!");
-  toggleFet1(); delay(200); toggleFet1(); // test and setting default states
-  toggleFet2(); delay(200); toggleFet2();
-  toggleFet3(); delay(200); toggleFet3();
 }
 
 void loop() {
   middleButton.tick();   // pump loop
   topButton.tick();
+
+  updateLights();
 }
 
 void middleButtonClick() {
   // one flash for fet1
   digitalWrite(LED_BUILTIN, HIGH); delay(100); digitalWrite(LED_BUILTIN, LOW);
 
-  toggleFet1();
+  // toggle
+  celingLightsEnabled = !celingLightsEnabled;
 }
 
 void topButtonClick() {
@@ -48,22 +66,27 @@ void topButtonClick() {
   digitalWrite(LED_BUILTIN, HIGH); delay(100); digitalWrite(LED_BUILTIN, LOW); delay(100);
   digitalWrite(LED_BUILTIN, HIGH); delay(100); digitalWrite(LED_BUILTIN, LOW);
   
-  toggleFet2();
+  // toggle
+  emergencyLightsEnabled = !emergencyLightsEnabled;
 }
 
-void toggleFet1() {
-  fet1High = !fet1High;
-  digitalWrite(outputPinFet1, fet1High ? HIGH : LOW);
+void updateLights() {
+  digitalWrite(outputPinFet1, celingLightsEnabled ? HIGH : LOW);
+
+  // flashing pattern from function array (really should be using interrupts)
+  if(emergencyLightsEnabled) {
+    emergencyLightsSequenceFP [emergencyLightsSequenceIndex++] ();
+    if (emergencyLightsSequenceIndex >= 12)
+      emergencyLightsSequenceIndex = 0;
+  }else{
+    digitalWrite(outputPinFet2, LOW);
+    digitalWrite(outputPinFet3, LOW);
+  }
 }
 
-void toggleFet2() {
-  fet2High = !fet2High;
-  digitalWrite(outputPinFet2, fet2High ? HIGH : LOW);  
-}
 
-void toggleFet3() {
-  fet3High = !fet3High;
-  digitalWrite(outputPinFet3, fet3High ? HIGH : LOW);  
-}
+
+
+
 
 
